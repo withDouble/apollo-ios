53014408bafcc65f12b22c8d16be9c6212bc67c3
Florent Bories <boriesflorent@gmail.com>
2024-12-09 17:21:53 -0800

    Update SQLiteDotSwiftDatabase.swift

--- Sources/ApolloSQLite/SQLiteDotSwiftDatabase.swift
+++ Sources/ApolloSQLite/SQLiteDotSwiftDatabase.swift
@@ -8,28 +8,28 @@ public final class SQLiteDotSwiftDatabase: SQLiteDatabase {
   private var db: Connection!
   
   private let records: Table
-  private let keyColumn: Expression<CacheKey>
-  private let recordColumn: Expression<String>
-  
+  private let keyColumn: SQLite.Expression<CacheKey>
+  private let recordColumn: SQLite.Expression<String>
+
   public init(fileURL: URL) throws {
     self.records = Table(Self.tableName)
-    self.keyColumn = Expression<CacheKey>(Self.keyColumnName)
-    self.recordColumn = Expression<String>(Self.recordColumName)
+    self.keyColumn = SQLite.Expression<CacheKey>(Self.keyColumnName)
+    self.recordColumn = SQLite.Expression<String>(Self.recordColumName)
     self.db = try Connection(.uri(fileURL.absoluteString), readonly: false)
   }
   
   public init(connection: Connection) {
     self.records = Table(Self.tableName)
-    self.keyColumn = Expression<CacheKey>(Self.keyColumnName)
-    self.recordColumn = Expression<String>(Self.recordColumName)
+    self.keyColumn = SQLite.Expression<CacheKey>(Self.keyColumnName)
+    self.recordColumn = SQLite.Expression<String>(Self.recordColumName)
     self.db = connection
   }
   
   public func createRecordsTableIfNeeded() throws {
     try self.db.run(self.records.create(ifNotExists: true) { table in
-      table.column(Expression<Int64>(Self.idColumnName), primaryKey: .autoincrement)
+      table.column(SQLite.Expression<Int64>(Self.idColumnName), primaryKey: .autoincrement)
       table.column(keyColumn, unique: true)
-      table.column(Expression<String>(Self.recordColumName))
+      table.column(SQLite.Expression<String>(Self.recordColumName))
     })
     try self.db.run(self.records.createIndex(keyColumn, unique: true, ifNotExists: true))
   }
